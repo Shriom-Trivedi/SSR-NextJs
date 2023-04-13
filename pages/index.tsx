b492ba8edc1ad3b70b9051f20d4dae32b7c70926
Shri Om Trivedi <shriom.trivedi@tridiagonal.com>
2023-04-13 17:52:10 +0530

    server side rendering done!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,39 @@
+import { Key } from "react";
 
-export default function Home() {
+export async function getServerSideProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+
+  return {
+    props: {
+      todos: data,
+    },
+  };
+}
+
+type TodosType = {
+  id: number;
+  title: string;
+};
+
+export default function Home({ todos }: { todos: TodosType[] }) {
+
   return (
-    <main>
-      
+    <main className="p-12">
+      {todos?.length === 0 ? (
+          <div>...Loading</div>
+        ) : (
+          todos?.map((todo: { id: Key; title: string }) => (
+            <div
+              key={todo.id}
+              className="p-3 rounded border mb-2 cursor-pointer hover:bg-[#e5e5e5]"
+            >
+              <p>
+                {todo.id}: {todo.title}
+              </p>
+            </div>
+          ))
+        )}
     </main>
-  )
+  );
 }
